cdcd929972532cb3ab0d3759b2afe087cf6ac0a4
hp <hp@DESKTOP-5FTUUVM>
2022-07-20 17:53:00 +0530

    [mahendra] added demo2

---
+++ src/sample1/demo2.java
@@ -0,0 +1,13 @@
+package sample1;
+
+public class demo2
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hi how are you");
+		
+		System.out.println("I am fine");
+		
+	}
+
+}
